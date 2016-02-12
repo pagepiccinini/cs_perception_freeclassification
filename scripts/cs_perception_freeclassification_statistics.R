d53b005bfbc7269b0f00942c1e3eabc2517b5eaf
Page Piccinini <page.piccinini@gmail.com>
2016-02-12 09:30:14 +0100

    Finishing cleaning up scripts for statistics.

--- scripts/cs_perception_freeclassification_statistics.R
+++ scripts/cs_perception_freeclassification_statistics.R
@@ -11,7 +11,9 @@ data_stats = data_clean %>%
   # Contrast code language of instruction (baseline set to English)
   mutate(Inst_LgContrast = ifelse(Inst_Lg == "E", -0.5, 0.5)) %>%
   # Contrast code language of stimuli (baseline set to English)
-  mutate(LanguageContrast = ifelse(Language == "E", -0.5, 0.5))
+  mutate(LanguageContrast = ifelse(Language == "E", -0.5, 0.5)) %>%
+  # Make slide number numeric
+  mutate(SlideNum = as.numeric(Slide))
 
 
 ## BUILD MODELS FOR ALL DATA ACROSS SLIDES ####
@@ -38,3 +40,51 @@ all_noinstlgxlanguage.glmer = glmer(Error ~ Inst_LgContrast * LanguageContrast -
                              (1+LanguageContrast|Subject), family = "binomial", data = data_stats)
 
 all_noinstlgxlanguage.anova = anova(all.glmer, all_noinstlgxlanguage.glmer)
+
+
+## BUILD MODELS FOR DATA BY SLIDE ####
+# Full model
+byslide.glmer = glmer(Error ~ Inst_LgContrast * SlideNum +
+                        (1+SlideNum|Subject) +
+                        (1|Sound.File), family = "binomial", data = data_stats)
+
+byslide.glmer_sum = summary(byslide.glmer)
+
+# Test for effect of language of instruction
+byslide_noinstlg.glmer = glmer(Error ~ Inst_LgContrast * SlideNum - Inst_LgContrast +
+                                 (1+SlideNum|Subject) +
+                                 (1|Sound.File), family = "binomial", data = data_stats)
+
+byslide_noinstlg.anova = anova(byslide.glmer, byslide_noinstlg.glmer)
+
+# Test for effect of slide number
+byslide_noslide.glmer = glmer(Error ~ Inst_LgContrast * SlideNum - SlideNum +
+                                 (1+SlideNum|Subject) +
+                                 (1|Sound.File), family = "binomial", data = data_stats)
+
+byslide_noslide.anova = anova(byslide.glmer, byslide_noslide.glmer)
+
+# Test for interaction of language of instruction x slide number
+byslide_noinstlgxslide.glmer = glmer(Error ~ Inst_LgContrast * SlideNum - Inst_LgContrast:SlideNum +
+                                 (1+SlideNum|Subject) +
+                                 (1|Sound.File), family = "binomial", data = data_stats)
+
+byslide_noinstlgxslide.anova = anova(byslide.glmer, byslide_noinstlgxslide.glmer)
+
+
+## FOLLOW UP SIMPLE REGRESSIONS TO TEST INTERACTION OF LANGUAGE OF INSTRUCTION X SLIDE NUMBER ####
+# Organize data
+data_eng_stats = data_stats %>%
+  filter(Inst_Lg == "E") %>%
+  mutate(Inst_Lg = factor(Inst_Lg))
+
+data_sp_stats = data_stats %>%
+  filter(Inst_Lg == "S") %>%
+  mutate(Inst_Lg = factor(Inst_Lg))
+
+# Run regressions
+eng.glm = glm(Error ~ SlideNum, family = "binomial", data = data_eng_stats)
+eng.glm_sum = summary(eng.glm)
+
+sp.glm = glm(Error ~ SlideNum, family = "binomial", data=data_sp_stats)
+sp.glm_sum = summary(sp.glm)
